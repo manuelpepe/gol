022982c20337983ca8267dbff809c17dac1a5681
Manuel Pepe <manuelpepe-dev@outlook.com.ar>
2024-05-05 21:49:18 -0300

    update gestures demo

--- demo/gesture.go
+++ demo/gesture.go
@@ -53,18 +53,29 @@ func (g *Gesture) Update() error {
 }
 
 func (g *Gesture) Draw(screen *ebiten.Image) {
-	status := ""
+	msgs := make([]string, 3)
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		vector.DrawFilledCircle(screen, float32(x), float32(y), 5, color.RGBA{0, 0, 255, 1}, true)
+		msgs = append(msgs, "left mouse button")
+	}
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+		x, y := ebiten.CursorPosition()
+		vector.DrawFilledCircle(screen, float32(x), float32(y), 5, color.RGBA{0, 255, 0, 1}, true)
+		msgs = append(msgs, "right mouse button")
+	}
+
 	if g.touch.IsTouchingThree() {
-		status = "touching three"
+		msgs = append(msgs, "touching three")
 	} else if g.touch.IsTouchingTwo() {
-		status = "touching two"
+		msgs = append(msgs, "touching two")
 		if pan := g.touch.Pan(); pan != nil {
-			status += " - pan"
+			msgs = append(msgs, "pan")
 			deltaX := pan.OriginX - pan.PrevX
 			if deltaX < -10 {
-				status += fmt.Sprintf(" - swipe right - delta: %.2f", deltaX)
+				msgs = append(msgs, fmt.Sprintf("swipe right - delta: %d", deltaX))
 			} else if deltaX > 10 {
-				status += fmt.Sprintf(" - swipe right - delta: %.2f", deltaX)
+				msgs = append(msgs, fmt.Sprintf("swipe left - delta: %d", deltaX))
 			}
 
 			vector.DrawFilledCircle(screen, float32(pan.OriginX), float32(g.h)/2, 5, color.RGBA{255, 0, 0, 1}, true)
@@ -73,25 +84,27 @@ func (g *Gesture) Draw(screen *ebiten.Image) {
 		}
 
 		if pinch := g.touch.Pinch(); pinch != nil {
-			status += "- pinch"
+			msgs = append(msgs, "pinch")
 		}
 	} else if g.touch.IsTouching() {
 		x, y, _ := g.touch.GetFirstTouchPosition()
-		status = "touching one"
+		msgs = append(msgs, "touching one")
 		vector.DrawFilledCircle(screen, float32(x), float32(y), 5, color.RGBA{0, 0, 255, 1}, true)
 	}
 
-	str := fmt.Sprintf("STATUS: %s", status)
-	textFace := &text.GoTextFace{
-		Source: mplusFaceSource,
-		Size:   24,
+	for ix, m := range msgs {
+		textFace := &text.GoTextFace{
+			Source: mplusFaceSource,
+			Size:   24,
+		}
+		w, h := text.Measure(m, textFace, 0)
+		textOp := &text.DrawOptions{}
+		textOp.GeoM.Translate(w/2+10, h*float64(ix+1))
+		textOp.PrimaryAlign = text.AlignCenter
+		textOp.SecondaryAlign = text.AlignCenter
+		text.Draw(screen, m, textFace, textOp)
 	}
-	_, h := text.Measure(str, textFace, 0)
-	textOp := &text.DrawOptions{}
-	textOp.GeoM.Translate(float64(g.w)/2, h)
-	textOp.PrimaryAlign = text.AlignCenter
-	textOp.SecondaryAlign = text.AlignCenter
-	text.Draw(screen, str, textFace, textOp)
+
 }
 
 func (g *Gesture) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
